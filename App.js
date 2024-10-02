073df18daa15fab0b904f349a2af28fb823b161b
manoj <manojkumar.paruchuri532@gmail.com>
2024-10-02 16:04:33 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child1",className:"heading1"},[
+        React.createElement("h1",{},"I'm an h1 element"),
+        React.createElement("h1",{},"I'm an h2 element")
+    ]),
+    React.createElement("div",{id:"child2",className:"heading2"},[
+        React.createElement("h1",{},"I'm an h1 element"),
+        React.createElement("h2",{},"I'm an h2 element")
+    ])
+])
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
+
+console.log(parent)
